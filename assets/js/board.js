5b89c95372b27bb1e0011fdf49177e07f7c14dc4
Farma20 <71374833+Farma20@users.noreply.github.com>
2021-07-28 20:50:45 +1000

    Add files via upload

--- assets/js/board.js
+++ assets/js/board.js
@@ -27,3 +27,75 @@ var Open_Special_Card = function() {
         open_close_special_card = 0;
     }
 }
+
+var id_card = 0;
+//функция добавления карт в руку
+var add_card_in_hand = function () {
+
+    //переменная, хранящая указатель на руку
+    let hend = document.getElementById("Hend");
+
+    //переменная с картой
+    let card = document.createElement("div");
+    card.className = "card card_in_hend";
+
+    card.onclick = Open_Info_Card;
+
+    hend.append(card);
+
+    //создание нового окна с действиями карты, которое открывается по нажатии
+    // let card_window = document.createElement("div");
+    // card_window.className += "card_window";
+    // card_window.innerHTML = '<div class="card"></div>\
+    // <div class="card_window_buttons">\
+    //     <!-- первый ряд кнопок -->\
+    //    <div class="line_buttons">\
+    //        <form>\
+    //            <button type="button">Применить</button>\
+    //        </form>\
+    //        <form>\
+    //             <button type="button">Вызвать</button>\
+    //         </form>\
+    //    </div>\
+    //    <!-- второй ряд кнопок -->\
+    //    <div class="line_buttons">\
+    //          <form>\
+    //              <button type="button">Надеть</button>\
+    //          </form>\
+    //         <form>\
+    //             <button type="button">В инвентарь</button>\
+    //         </form>\
+    //     </div>\
+    //     <!-- третий ряд кнопок -->\
+    //     <div class="line_buttons">\
+    //         <form>\
+    //             <button type="button">Продать</button>\
+    //         </form>\
+    //         <form>\
+    //              <button type="button">Украсть</button>\
+    //          </form>\
+    //     </div>\
+    //     <!-- четвертый ряд кнопок -->\
+    //     <div class="line_buttons">\
+    //         <form>\
+    //             <button type="button">Сбросить</button>\
+    //         </form>\
+    //         <form>\
+    //              <button type="button">Взять в руку</button>\
+    //          </form>\
+    //     </div>\
+    //     <!-- кнопка закрытия -->\
+    //     <div class="div_exit_button">\
+    //         <form>\
+    //             <button type="button" onclick="Open_Info_Card()">Закрыть</button>\
+    //         </form>\
+    //     </div>\
+    // </div>'
+    // id_card++;
+
+}
+
+//функция выбрасывания карты
+var drop_card_out_henad = function () {
+
+}
